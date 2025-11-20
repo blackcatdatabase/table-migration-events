ea6eda11dea9cb52660444c6307ca6cd65857b07
Tolben <vitek.pasek@gmail.com>
2025-11-20 22:18:35 +0100

    regen schema

--- schema/020_indexes.postgres.sql
+++ schema/020_indexes.postgres.sql
@@ -1,4 +1,4 @@
--- Auto-generated from schema-map-postgres.psd1 (map@9d3471b)
+-- Auto-generated from schema-map-postgres.psd1 (map@62c9c93)
 -- engine: postgres
 -- table:  migration_events
 CREATE INDEX IF NOT EXISTS idx_mig_system_status ON migration_events (system_name, status);
